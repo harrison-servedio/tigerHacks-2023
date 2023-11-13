95d9fe5689025f6e2ae1c17ff71453ff40b07ecf
hs242424 <79582976+harrison-servedio@users.noreply.github.com>
2023-11-13 12:45:20 -0500

    Uploaded q 4

---
+++ Question 4 - The Maître D’s Revenge/solution.py
@@ -0,0 +1,20 @@
+directions = list(map(int, input().split()))
+m = list(map(int, input().split()))
+fridge = []
+for i in range(9):
+    fridge.append(input())
+fridge = [i.split("; ") for i in fridge]
+
+x, y = 0, 0
+steps = 0
+
+for i in m:
+    for j in range(i):
+        a = steps + j
+        dir = directions[a]
+        if a % 2 == 0:
+            x += dir
+        else:
+            y -= dir
+    steps += i
+    print(fridge[y % 9][x % 9])
